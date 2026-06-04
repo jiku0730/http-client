c48e5170fcbab85dc59adbcac196754e12d87bb1
jiku0730 <github@jiku.jp>
2026-06-04 13:15:35 +0900

    feat: socketを作成。

--- socket.c
+++ socket.c
@@ -1,7 +1,17 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/socket.h>
 
 int main(void)
 {
   printf("Hello World!\n");
+  int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+  {
+    printf("fail to make socket.\n");
+    return (1);
+  }
+  printf("success to make socket!\n");
+  close(sockfd);
   return (0);
 }
